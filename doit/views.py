853059fedc49658cb1a8b2177637140304024e89
Kushaga Agnihotri <68841296+kushagra-aa@users.noreply.github.com>
2021-08-13 15:16:30 +0000

    added index.html and a form

--- doit/views.py
+++ doit/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-  return render(request,'base.html')
+  return render(request,'doit/index.html')
+
+def add_todo(request):
+  print(request)
