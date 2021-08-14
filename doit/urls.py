6f387c06cc54ec5362ca7e7bc4aa29dcac9fa03b
Kushaga Agnihotri <68841296+kushagra-aa@users.noreply.github.com>
2021-08-14 17:59:19 +0000

    completed all basic funtionality

--- doit/urls.py
+++ doit/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home,name='home'),    
-    path('add_todo/',views.add_todo,name='add_todo')
+    path('add_todo/',views.add_todo,name='add_todo'),
+    path('delete_todo/<int:todo_id>/',views.delete_todo,name='delete_todo')
 ]
--- doit/views.py
+++ doit/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseRedirect
+from django.utils import timezone
+from .models import Todo
 
 # Create your views here.
 def home(request):
-  return render(request,'doit/index.html')
-
+  todo_items = Todo.objects.all().order_by("-added_date")
+  return render(request,'doit/index.html',{
+    "todo_items":todo_items
+  })
+@csrf_exempt
 def add_todo(request):
-  print(request)
+  added_date=timezone.now()
+  todo= request.POST["todo"]
+  created_obj = Todo.objects.create(added_date=added_date,text=todo)
+  print(created_obj)
+  return HttpResponseRedirect('/')
+
+@csrf_exempt
+def delete_todo(request, todo_id):
+  Todo.objects.get(id=todo_id).delete()
+  return HttpResponseRedirect('/')
